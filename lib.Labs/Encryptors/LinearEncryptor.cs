b964cd42cfd5a388f99b7baa5ff8d2807df3235e
flores666 <zatworld3@gmail.com>
2023-09-26 01:08:01 +0300

    bugs fixed

--- lib.Labs/Encryptors/LinearEncryptor.cs
+++ lib.Labs/Encryptors/LinearEncryptor.cs
@@ -16,45 +16,34 @@ public class LinearEncryptor : EncryptorBase
     public override string Encrypt(string input)
     {
         var resultBytes = new List<byte>();
-
+        var sequenceIndex = 0;
         for (var i = 0; i < input.Length; i++)
         {
             var ch = input[i];
             var bytes = Encoding.UTF8.GetBytes(new char[] {ch});
             for (var j = 0; j < bytes.Length; j++)
             {
-                var code = (byte) ((bytes[j] + _sequence[i]) % B);
+                var code = (byte) ((bytes[j] + _sequence[sequenceIndex++]) % B);
                 resultBytes.Add(code);
             }
         }
 
-        byte[] encryptedBytes = resultBytes.ToArray();
-        return Convert.ToBase64String(encryptedBytes);
+        return Convert.ToBase64String(resultBytes.ToArray());
     }
 
 
     public override string Decrypt(string input)
     {
         byte[] encryptedBytes = Convert.FromBase64String(input);
-        var result = new StringBuilder();
-        var code = new List<byte>();
-        var sequenceIndex = 0;
+        var codes = new List<byte>();
+
         for (var i = 0; i < encryptedBytes.Length; i++)
         {
-            code.Add((byte)(encryptedBytes[i] - _sequence[sequenceIndex]));
-            
-            if (code[0] < 0) code[0] = (byte)(code[0] + B);
-            else if (code[0] is 208 or 209)
-            {
-                code.Add((byte)(encryptedBytes[++i] - _sequence[sequenceIndex]));
-            } 
-            
-            sequenceIndex = (sequenceIndex + 1) % _sequence.Count;
-            result.Append(Encoding.UTF8.GetChars(code.ToArray()));
-            code.Clear();
+            codes.Add((byte) (encryptedBytes[i] - _sequence[i]));
+            if (codes[i] < 0) codes[i] = (byte) (codes[i] + B);
         }
 
-        return result.ToString();
+        return Encoding.UTF8.GetString(codes.ToArray());
     }
 
     private List<int> GenerateSequence(int length)
@@ -63,7 +52,8 @@ public class LinearEncryptor : EncryptorBase
         var C = 39;
         var result = new List<int> {41};
         var i = 1;
-        while (result.Count != length)
+        var maxBytesInSymbol = 4;
+        while (result.Count != length * maxBytesInSymbol)
         {
             result.Add((A * result[i - 1] + C) % B);
             i++;
