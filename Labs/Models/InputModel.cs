1d6f5283572cee93914bb56b2322f70f5d931b9a
Васильев Егор <regenerationdream@gmail.com>
2023-09-30 10:15:59 +0300

    Updated site style

--- Labs/Models/InputModel.cs
+++ Labs/Models/InputModel.cs
@@ -1,14 +1,17 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using Laba1.Extensions;
 
 namespace Laba1.Models;
 
 public class InputModel
 {
     [Required]
+    [DisplayName("Ввод")]
     public string Input { get; set; }
     
+    [DisplayName("Ключ")]
     public string? Key { get; set; }
     
+    [DisplayName("Вывод")]
     public string? Output { get; set; }
 }
