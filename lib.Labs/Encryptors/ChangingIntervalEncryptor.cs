e7bdc2a924482b3c97d4cff5f7f8a306c9b7402e
flores666 <regenerationdream@gmail.com>
2024-03-22 09:15:08 +0300

    Оптимизиация дешифратора лаб8

--- lib.Labs/Encryptors/ChangingIntervalEncryptor.cs
+++ lib.Labs/Encryptors/ChangingIntervalEncryptor.cs
@@ -56,42 +56,21 @@ public class ChangingIntervalEncryptor : EncryptorBase, ISteganography
 
     public override string Decrypt(string container)
     {
-        var messageBuilder = new StringBuilder();
-
-        var sentences = container.Split('.', '!', '?');
-        bool isEndReached = false;
-
-        for (int i = 0; i < sentences.Length; i++)
+        var encodedBits = new StringBuilder();
+        var index = 0;
+        
+        for (int i = 0; i < container.Length; i++)
         {
-            if (sentences[i].Contains(END_MARKER))
+            if (container[i] == END_MARKER) break;
+            if (IsEndOfSentence(container[i]))
             {
-                isEndReached = true;
-                // Находим маркер окончания сообщения и обрезаем сообщение
-                var markerIndex = sentences[i].IndexOf(END_MARKER);
-                if (markerIndex != -1)
-                {
-                    sentences[i].Remove(markerIndex, sentences[i].Length - markerIndex);
-                }
+                if (i >= 2 && container[i - 2] == ' ') encodedBits.Append('0');
+                else encodedBits.Append('1');
+                if (++index % 16 == 0) encodedBits.Append(' ');
             }
-
-            if (!isEndReached)
-            {
-                if (sentences[i].EndsWith("  "))
-                {
-                    messageBuilder.Append('0');
-                }
-                else if (sentences[i].EndsWith(" "))
-                {
-                    messageBuilder.Append('1');
-                }
-            }
-
-            if ((i + 1) % 16 == 0) messageBuilder.Append(" ");
         }
-
-        var message = messageBuilder.ToString().TrimEnd();
-
-        return BinaryToString(message);
+        
+        return BinaryToString(encodedBits.ToString().TrimEnd());
     }
 
     private static bool IsEndOfSentence(char ch)
