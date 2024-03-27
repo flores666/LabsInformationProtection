6a69b0ac282a17f2bd283422b314a152909659d5
flores666 <regenerationdream@gmail.com>
2024-03-27 11:56:20 +0300

    Работчающая 9.1 лаба

--- lib.Labs/Encryptors/ChangingNumberOfSpacesEncryptorBits.cs
+++ lib.Labs/Encryptors/ChangingNumberOfSpacesEncryptorBits.cs
@@ -5,51 +5,58 @@ namespace lib.Labs.Encryptors;
 
 public class ChangingNumberOfSpacesEncryptorBits : EncryptorBase, ISteganographyBits
 {
+    private const char END_MARKER = '@'; // Маркер окончания сообщения
+
     public string Container { get; set; }
     public int BitsNumber { get; set; }
 
     public override string Encrypt(string input)
     {
-        if (!LengthEnough(Container, input.Length * 16))
+        if (!LengthEnough(Container, (input.Length * 16) / BitsNumber))
         {
             return "Не хватает длины контейнера, увеличьте его";
         }
-        
-        var result = new StringBuilder(Container.Length + input.Length * 2);
 
-        int k = 0;
-        for (int i = 0, j = 0; i < input.Length; k++)
+        // Преобразование сообщения в бинарный формат
+        var binaryMessage = new StringBuilder();
+        foreach (char c in input)
         {
-            if (Container[k] == '\n')
-            {
-                for (int z = 0; i < input.Length && z < BitsNumber; z++)
-                {
-                    bool isZeroBit = (input[i] & ((char)1 << 15 - j)) == 0;
-                    if (isZeroBit)
-                        result.Append(' ');
-                    else
-                        result.Append((char)160); // ascii Non-breaking space
-
-                    j++;
-
-                    if (j == 16)
-                    {
-                        j = 0;
-                        i++;
-                    }
-                }
-            }
-
-            result.Append(Container[k]);
+            binaryMessage.Append(Convert.ToString(c, 2).PadLeft(16, '0'));
         }
 
-        result.Append(Container[k]);
-        k++;
-        result.Append(Container.AsSpan(k..));
+        PreprocessContainer(binaryMessage.Length);
 
-        return result.ToString();
+        // Размещение сообщения в контейнере
+        var stegoContainer = new StringBuilder();
+        var binaryMessageIndex = 0;
+        var totalSpacesCount = 0;
+        
+        foreach (char ch in Container)
+        {
+            if (ch == '\n')
+            {
+                for (int i = 0; binaryMessageIndex < binaryMessage.Length && i < BitsNumber; i++)
+                {
+                    var insertChar = binaryMessage[binaryMessageIndex++] == '1' ? (char)160 : ' ';
+                    stegoContainer.Append(insertChar);
+                    totalSpacesCount++;
+                }
+            }
+            
+            stegoContainer.Append(ch);
+
+            if (binaryMessageIndex == binaryMessage.Length)
+            {
+                stegoContainer.Append(END_MARKER);
+                stegoContainer.Append(Container.AsSpan(stegoContainer.Length - totalSpacesCount - 1, 
+                    Container.Length - (stegoContainer.Length - totalSpacesCount) - 1));
+                break;
+            }
+        }
+
+        return stegoContainer.ToString();
     }
-
+    
     public override string Decrypt(string input)
     {
         List<byte> encodedBits = new(16);
@@ -89,6 +96,28 @@ public class ChangingNumberOfSpacesEncryptorBits : EncryptorBase, ISteganography
         return new string(encodedChars);
     }
 
+    /*public override string Decrypt(string input)
+    {
+        var encodedBits = new StringBuilder();
+        var index = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == END_MARKER) break;
+            if (input[i] == '\n')
+            {
+                for (int j = 1; j <= BitsNumber; j++)
+                {
+                    if (input[i - j] == ' ') encodedBits.Append('0');
+                    else encodedBits.Append('1');
+                    if (++index % 16 == 0) encodedBits.Append(' ');
+                }
+            }
+        }
+
+        return BinaryToString(encodedBits.ToString().TrimEnd());
+    }*/
+
     private bool LengthEnough(string container, int inputLength)
     {
         var counter = 0;
@@ -99,5 +128,44 @@ public class ChangingNumberOfSpacesEncryptorBits : EncryptorBase, ISteganography
         }
 
         return false;
+    }
+
+    private void PreprocessContainer(int rowsToProcess)
+    {
+        var rowIndex = 0;
+
+        for (var i = 0; rowIndex < rowsToProcess; i++)
+        {
+            if (Container[i] != '\n') continue;
+
+            var j = i;
+
+            while (Container[j] == ' ' || Container[j] == (char)160)
+            {
+                j--;
+            }
+
+            Container = Container.Remove(j, i - j);
+
+            rowIndex++;
+        }
+    }
+
+    private string BinaryToString(string binary)
+    {
+        var sb = new StringBuilder();
+
+        var binaryValues = binary.Split(' ');
+        foreach (string binaryValue in binaryValues)
+        {
+            // Преобразование двоичной строки в целое число
+            var asciiValue = Convert.ToInt32(binaryValue, 2);
+
+            // Преобразование ASCII-кода в символ и добавление его к результату
+            var character = (char)asciiValue;
+            sb.Append(character);
+        }
+
+        return sb.ToString();
     }
 }
